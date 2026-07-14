fb2204f7856ddba354036f8245449cca7a728530
puneeththota009-web <puneeth.thota009@gmail.com>
2026-07-14 08:09:05 -0400

    Update GradeItem.java

--- src/GradeItem.java
+++ src/GradeItem.java
@@ -1,15 +1,17 @@
 package src;
 public class GradeItem {
     private String title;
-     private double score;
-     public GradeItem(String title, double score){
+    private double score;
+    public GradeItem(String title, double score){
+        this.title = title;
+        this.score = score;
         if (title == null || title.trim().isEmpty()) {
         throw new IllegalArgumentException("Title cannot be empty.");
         }
-        if (score < 0 || score > 100){
+        if (score < 0 || score > 100) {
             throw new IllegalArgumentException("Score is out of bounds");
         }
-     }
+    }
     public String getTitle(){
         return title;
     }
